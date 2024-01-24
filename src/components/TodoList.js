93e3ba5850511dfa9115fbfe9a3b30dbfff44ebe
jagadeeshwar011 <jagadeeshwarreddy011@gmail.com>
2024-01-24 17:55:06 -0600

    Updated git files

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,6 +8,7 @@ function Todolist(props) {
         <i className="fa-solid fa-trash-can icon-delete" 
         onClick={e=>{
             props.deleteItem(props.index)
+            
         }}></i>
         </span>
     </li>
